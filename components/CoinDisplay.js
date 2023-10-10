531c041ae404ecd9dadd042413ca0b6636109af5
SumitParmar84 <136489026+SumitParmar84@users.noreply.github.com>
2023-10-10 23:25:27 +0530

    Update CoinDisplay.js

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -3,13 +3,25 @@ import CoinCard from "./CoinCard";
 
 function CoinDisplay({ coin }) {
   const [coins, setCoins] = useState([]);
+  useEffect(()=>{
+    fetch("https://api.coinlore.net/api/tickers/")
+    .then((response)=>response.json())
+    .then((res)=>{
+      let sort = res.data.sort((a, b) => a.rank-b.rank);
+      sort = sort.slice(0,20)
+      setCoins(sort)
+      console.log(sort)
+      })
+    .catch(err=>console.log(err))
 
+    setCoins()
+  },[])
   return (
     <div className="home">
       <h1>Top 20 Cryptos</h1>
       <div className="coins-container">
-        {coins.map((coin) => (
-          <CoinCard />
+        {coins && coins.map((coin) => (
+          <CoinCard coin={coin} />
         ))}
       </div>
     </div>
